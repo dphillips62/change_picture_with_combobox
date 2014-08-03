7417d2081ebd2474ea55e08ba54d313e7f0318ad
dphillips62 <dphillips@newington.nsw.edu.au>
2014-08-03 19:02:25 +1000

    Select box moved to before image

--- mysrc.js
+++ mysrc.js
@@ -2,8 +2,8 @@
  * Created by dphillips on 3/08/2014.
  */
 
-function xxx(select){
-    console.log(select);
-    document.getElementById("myimage").src="./Images/"+select+".jpeg";
+function changeimage(thevalue){
+    console.log(thevalue);
+    document.getElementById("myimage").src="./Images/"+thevalue+".jpeg";
 }
 
